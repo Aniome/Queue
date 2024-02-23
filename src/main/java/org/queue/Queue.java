447d7a167cee212fe440348bf41a1294a72d6620
Aniome <vaganov.kostya2000@yandex.ru>
2024-02-23 21:02:42 +0500

    Add queue class

--- src/main/java/org/queue/Queue.java
+++ src/main/java/org/queue/Queue.java
@@ -1,2 +1,32 @@
-package org.queue;public class Queue {
+package org.queue;
+
+public class Queue {
+    private int[] queue;
+    private int ind;
+    public Queue(int length){
+        queue = new int[length];
+    }
+
+    public int push(int data){
+        queue[ind] = data;
+        ind++;
+        return 0;
+    }
+
+    public int pop(){
+        int data = queue[0];
+        ind--;
+        for (int i = 0; i < ind; i++){
+            queue[i] = queue[i + 1];
+        }
+        return data;
+    }
+
+    public boolean isEmpty(){
+        return ind == 0;
+    }
+
+    public boolean isFull(){
+        return ind == queue.length;
+    }
 }
